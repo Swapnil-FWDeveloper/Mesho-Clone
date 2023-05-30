480ca323b0d5dc3fb13d4859a1938475042ef3ba
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2023-05-30 23:32:10 +0530

    refacter Mesho Footer

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
