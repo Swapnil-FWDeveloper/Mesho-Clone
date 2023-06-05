a335b067e3ad699b2c893b40c2bf7f3987f2cb23
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2023-06-05 23:39:44 +0530

    adding new pages

--- my-app/src/Routes/AllRoutes.jsx
+++ my-app/src/Routes/AllRoutes.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Pages/Home'
+import Address from '../Pages/Address'
+import Cart from '../Pages/Cart'
+import Login from '../Pages/Login'
+import Payment from '../Pages/Payment'
+import Search from '../Pages/Search'
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path="/Address" element={<Address/>}/>
+    <Route path="Cart" element={<Cart/>}/>
+    <Route path='Login' element={<Login/>}/>
+    <Route path='Payment' element={<Payment/>}/>
+    <Route path='Search' element={<Search/>}/>
+    
+    </Routes>
   )
 }
 
